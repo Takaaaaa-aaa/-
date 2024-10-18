ad0ff1aba6dbd6dec9c53bef1fb085568359ee97
Takaaaaa-aaa <takahito.abe0310@gmail.com>
2024-10-18 19:27:15 +0900

    Add files via upload

---
+++ cats2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+	int a,b,c;
+    printf("How many cats?:");
+    scanf("%d",&a);
+	printf("How many dogs?");
+	scanf("%d",&b);
+	c=a+b;
+    printf("animals is %d",c);
+
+    return 0;
+}
