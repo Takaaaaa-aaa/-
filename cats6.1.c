a29d63cd58861347a48921e295f4d2839d5ea540
Takaaaaa-aaa <takahito.abe0310@gmail.com>
2024-11-15 19:04:50 +0900

    Add files via upload

---
+++ cats6.1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int Cat,neko,tama;
+    
+    if(Cat==20)
+        printf("太郎は２０歳です\n");
+    if(neko != 20)
+        printf("花子は２０歳ではありません\n");
+    if(neko >= 20)
+        printf("花子は２０歳以上です\n");
+    if(Cat < 20)
+        printf("次郎は２０歳未満です\n");
+    
+    if(Cat > tama)
+        printf("太郎は次郎より年上です\n");
+    if(neko > Cat)
+        printf("花子は太郎より年上です\n");
+        
+        return 0;
+}
